1db15d54d9ce6553801d9e30e7115ad9180018b5
eddyme23 <saxonyedward@gmail.com>
2025-12-28 21:26:28 +0000

    Update full.sh

--- full.sh
+++ full.sh
@@ -18,7 +18,7 @@ Squid_Port1='3128'
 Squid_Port2='8080'
 
 # Python Socks Proxy
-WsPorts=('80' '8080' '8880' '2086' '2082' '25') # for port 8080 change cloudflare SSL/TLS to full
+WsPorts=('80' '8080' '8880' '2086' '2082') # for port 8080 change cloudflare SSL/TLS to full
 WsPort='80'                      # single backend WS port used by the proxy (keeps other ports redirected to this)
 WsResponse='HTTP/1.1 101 Switching AustroPlus Protocols\r\n\r\n'
 
@@ -625,7 +625,6 @@ iptables -t nat -A PREROUTING -p tcp --dport 8080 -j REDIRECT --to-port $WsPort
 iptables -t nat -A PREROUTING -p tcp --dport 8880 -j REDIRECT --to-port $WsPort
 iptables -t nat -A PREROUTING -p tcp --dport 2082 -j REDIRECT --to-port $WsPort
 iptables -t nat -A PREROUTING -p tcp --dport 2086 -j REDIRECT --to-port $WsPort
-iptables -t nat -A PREROUTING -p tcp --dport 25   -j REDIRECT --to-port $WsPort
 
 # Nginx configure
 rm /home/vps/public_html -rf
@@ -710,7 +709,7 @@ cat <<'mySquid' > /etc/squid/squid.conf
 # My Squid Proxy Server Config
 acl server dst IP-ADDRESS/32 localhost
 acl checker src 188.93.95.137
-acl ports_ port 14 22 53 21 8080 8081 8000 3128 1193 1194 440 441 442 299 550 790 443 80
+acl ports_ port 14 22 53 21 8080 8081 8880 8000 3128 1193 1194 440 441 442 299 550 790 443 80
 http_port Squid_Port1
 http_port Squid_Port2
 access_log none
@@ -1032,7 +1031,7 @@ chmod 755 /etc/hysteria/config.json
 chmod 755 /etc/hysteria/hysteria.crt
 chmod 755 /etc/hysteria/hysteria.key
 
-iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 20000:50000 -j DNAT --to-destination :5666
+iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 20000:50000 -j DNAT --to-destination :36712
 systemctl enable hysteria-server.service
 systemctl restart hysteria-server.service
 systemctl status --no-pager hysteria-server.service
@@ -1056,7 +1055,6 @@ iptables -t nat -A PREROUTING -p tcp --dport 8080 -j REDIRECT --to-port MyWSPort
 iptables -t nat -A PREROUTING -p tcp --dport 8880 -j REDIRECT --to-port MyWSPort
 iptables -t nat -A PREROUTING -p tcp --dport 2082 -j REDIRECT --to-port MyWSPort
 iptables -t nat -A PREROUTING -p tcp --dport 2086 -j REDIRECT --to-port MyWSPort
-iptables -t nat -A PREROUTING -p tcp --dport 25   -j REDIRECT --to-port MyWSPort
 
 # Disable IpV6
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
@@ -1071,7 +1069,7 @@ touch /var/run/sslh/sslh.pid
 chmod 777 /var/run/sslh/sslh.pid
 
 # For udp
-iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 20000:50000 -j DNAT --to-destination :5666
+iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 20000:50000 -j DNAT --to-destination :36712
 
 deekayz
 
